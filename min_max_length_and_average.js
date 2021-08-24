4dc9f1a195b24584fde3aece09e257d47062ff0d
Solea83 <sophia.vonbank@icloud.com>
2021-08-24 18:55:06 +0200

    Min, Max, Length and Average

---
+++ min_max_length_and_average.js
@@ -0,0 +1,11 @@
+//Write a function that converts an object into an array of keys and values using a simple for loop.
+
+function objectToArray(obj) {
+    let resultArray = [];
+    for (const key in obj) {
+        let newArr = [key, obj[key]];
+        resultArray.push(newArr);
+    }
+    return resultArray;
+};
+console.log(objectToArray({likes: 2, dislikes: 3, followers: 10})); // [["likes", 2], ["dislikes", 3], ["followers", 10]]
